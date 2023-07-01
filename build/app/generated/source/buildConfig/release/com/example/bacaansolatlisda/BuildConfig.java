e261085c0a28bd2153d2a7dac4f17bf63d682dbf
Lisda  Nurhaqiqi <2006200@itg.ac.id>
2023-07-01 12:34:41 +0700

    commen build

---
+++ build/app/generated/source/buildConfig/release/com/example/bacaansolatlisda/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.bacaansolatlisda;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.bacaansolatlisda";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
